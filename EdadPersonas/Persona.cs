2b365f5c6461e6a535d4b487ba769d1e96bb3809
Ricardo Rivas <richistron@gmail.com>
2022-09-23 21:53:17 -0700

    first draft

---
+++ EdadPersonas/Persona.cs
@@ -0,0 +1,24 @@
+﻿namespace EdadPersonas;
+
+public class Persona
+{
+  private int edad;
+  private string nombre;
+  private const int mayoriaEdad = 18;
+
+  public Persona(string _nombre, int _edad)
+  {
+    nombre = _nombre;
+    edad = _edad;
+  }
+
+  public string Nombre
+  {
+    get => nombre;
+  }
+
+  public bool EsMayorDeEdad()
+  {
+    return edad > mayoriaEdad;
+  }
+}
---
+++ EdadPersonas/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// Desarrollar un programa que permita cargar por teclado  5 nombres de personas y sus edades respectivas. 
+// Luego mostrar en pantalla los nombres de las personas mayores de edad 
+
+using EdadPersonas;
+
+NombresPersonas nombresPersonas = new NombresPersonas(5);
+nombresPersonas.CapturaLosNombres();
+nombresPersonas.MayoresDeEdad();
